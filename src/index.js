1610b400b3da65bad8a1bca54ddd457fa0e43c82
Tishana B. <t.bascombe@outlook.com>
2025-07-18 16:30:38 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// JS Challenge 1
+
+axios.get(url).then(displayLocationTemperature);
+
+// JS Challenge 2
+
+function displayLocationTemperature(response) {
+    console.log(response)
+}
+
+let url = https://api.shecodes.io/weather/v1/current?query=Lisbon&key=68b934o4bafcaf00b4452c44bfc46ct3&units=metric;
