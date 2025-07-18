866e47f54ba056d84e0ff408c7be65e486b25f90
Tishana B. <t.bascombe@outlook.com>
2025-07-18 21:07:13 +0000

    refactor displayLocationTemperature function code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ function displayLocationTemperature(response) {
   let formattedTemperature = Math.round(response.data.temperature.current);
   console.log(formattedTemperature);
 
+  let city = "Sydney";
+
   let h1 = document.querySelector("h1");
   h1.innerHTML = `The temperature of ${city} is currently ${formattedTemperature}ºC!`;
 }
 
-let city = "Sydney";
-
 const apiKey = "68b934o4bafcaf00b4452c44bfc46ct3";
-const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+const apiUrl = `https://api.shecodes.io/weather/v1/current?query=Sydney&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayLocationTemperature);
