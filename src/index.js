452515b5e90ca202b0ff1d53b7c9f6db3e09a870
Tishana B. <t.bascombe@outlook.com>
2025-07-18 20:54:23 +0000

    add API functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-// JS Challenge 1
+function displayLocationTemperature(response) {
+  let formattedTemperature = Math.round(response.data.temperature.current);
+  console.log(formattedTemperature);
+
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `The temperature of ${city} is currently ${formattedTemperature}ºC!`;
+}
+
+let city = "Sydney";
+
+const apiKey = "68b934o4bafcaf00b4452c44bfc46ct3";
+const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayLocationTemperature);
-// const apiUrl =
